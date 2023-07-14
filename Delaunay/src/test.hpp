3b8046cd80e0d8f3a6e6ba0c5e07353df03ad82c
glperini <126665786+glperini@users.noreply.github.com>
2023-07-14 09:32:40 +0200

    Update test.hpp

--- Delaunay/src/test.hpp
+++ Delaunay/src/test.hpp
@@ -1,18 +1,1 @@
-#ifndef __TEST_DELAUNAY_H
-#define __TEST_DELAUNAY_H
-
-#include <gtest/gtest.h>
-#include "iostream"
-#include "empty_class.hpp"
-
-TEST(TestSorting, TestMergeSort)    //DA RIVEDERE NOMI!!!!
-{
-  vector<int> v = {44, 25, 10, 31, 25, 48, 37, 43, 18, 48, 27};
-  MergeSort<int>(v, 0, v.size()-1); 
-  vector<int> sortedV = {10, 18, 25, 25, 27, 31, 37, 43, 44, 48, 48};
-  EXPECT_EQ(v, sortedV);
-}
-
-TEST()
-
 
